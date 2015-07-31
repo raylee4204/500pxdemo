bd03eb96abb4cf0f6b0340ee1c209435fc729cbf
Kanghee Lee <raylee4204@gmail.com>
2015-07-31 15:16:04 -0400

    Set up basic grid view for thumbnails

--- Demo500Px/app/src/main/java/com/example/demo500px/fragment/MainFragment.java
+++ Demo500Px/app/src/main/java/com/example/demo500px/fragment/MainFragment.java
@@ -2,16 +2,21 @@ package com.example.demo500px.fragment;
 
 import com.example.demo500px.R;
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 /**
- * A placeholder fragment containing a simple view.
+ * A fragment containing a grid of thumbnails
  */
-public class MainFragment extends Fragment {
+public class MainFragment extends BaseFragment {
+
+    private RecyclerView mRecyclerView;
 
     public MainFragment() {
     }
@@ -19,6 +24,14 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        getSupportActivity().setSupportActionBar((Toolbar) view.findViewById(R.id.toolbar));
+        getSupportActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.grid_thumbnails);
+        int numColumns = getResources().getInteger(R.integer.num_columns_thumbnail);
+        GridLayoutManager layoutManager = new GridLayoutManager(getSupportActivity(), numColumns);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        return view;
     }
 }
