a23c7561dfa9026df9eb7fd3117f2409512f3b3d
Kanghee Lee <raylee4204@gmail.com>
2015-07-31 15:15:30 -0400

    Use AppCompat version of Activity

--- Demo500Px/app/src/main/java/com/example/demo500px/activity/MainActivity.java
+++ Demo500Px/app/src/main/java/com/example/demo500px/activity/MainActivity.java
@@ -2,12 +2,12 @@ package com.example.demo500px.activity;
 
 import com.example.demo500px.R;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
