cdad0c77f102a9aa628c2cb79d31f3edd00b4302
Kanghee Lee <raylee4204@gmail.com>
2015-08-03 18:35:34 -0400

    Main: Clean up resources and change how fragment is attached

--- Demo500Px/app/src/main/java/com/example/demo500px/activity/MainActivity.java
+++ Demo500Px/app/src/main/java/com/example/demo500px/activity/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.demo500px.activity;
 
 import com.example.demo500px.R;
+import com.example.demo500px.fragment.MainFragment;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,28 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment, new MainFragment())
+                .commit();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
