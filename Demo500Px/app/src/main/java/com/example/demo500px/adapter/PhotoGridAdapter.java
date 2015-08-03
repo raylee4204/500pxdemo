ac21c1372d0936db6e8b073705dca95f89e7350a
Kanghee Lee <raylee4204@gmail.com>
2015-08-03 18:47:07 -0400

    Create adapter to parse handle image loading of Photos

---
+++ Demo500Px/app/src/main/java/com/example/demo500px/adapter/PhotoGridAdapter.java
@@ -0,0 +1,80 @@
+package com.example.demo500px.adapter;
+
+import com.example.demo500px.R;
+import com.example.demo500px.network.model.Photo;
+import com.squareup.picasso.Picasso;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+/**
+ * @author Kanghee
+ */
+public class PhotoGridAdapter extends BaseRecyclerGridAdapter<PhotoGridAdapter.PhotoViewHolder> {
+
+    private Context mContext;
+    private LayoutInflater mInflater;
+    private ArrayList<Photo> mPhotos;
+
+    public PhotoGridAdapter(Context context, ArrayList<Photo> photos) {
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+        mPhotos = photos;
+    }
+
+    @Override
+    public PhotoViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = mInflater.inflate(R.layout.item_photo, viewGroup, false);
+        final PhotoViewHolder viewHolder = new PhotoViewHolder(view);
+        viewHolder.itemView.setLayoutParams(mImageViewLayoutParams);
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mItemClickListener.onItemClick(v, viewHolder.getAdapterPosition());
+            }
+        });
+        return viewHolder;
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mNumColumns == 0) {
+            return 0;
+        }
+
+        return mPhotos.size();
+    }
+
+    @Override
+    public void onBindViewHolder(PhotoViewHolder viewHolder, int position) {
+        Photo photo = mPhotos.get(position);
+
+        if (mItemHeight > 0) {
+            Picasso.with(mContext)
+                    .load(photo.getSmallImageUrl())
+                    .resize(mItemHeight, mItemHeight)
+                    .into(viewHolder.photoView);
+        } else {
+            Picasso.with(mContext)
+                    .load(photo.getSmallImageUrl())
+                    .fit()
+                    .into(viewHolder.photoView);
+        }
+    }
+
+    static class PhotoViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView photoView;
+
+        public PhotoViewHolder(View itemView) {
+            super(itemView);
+            photoView = (ImageView) itemView.findViewById(R.id.img_photo);
+        }
+    }
+}
