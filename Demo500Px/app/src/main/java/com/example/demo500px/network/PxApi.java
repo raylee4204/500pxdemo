226e6772c45d72220925b3187dd97a4d9d13e840
Kanghee Lee <raylee4204@gmail.com>
2015-08-03 18:44:00 -0400

    PxAPI: Add method to process `GET photos` API call

--- Demo500Px/app/src/main/java/com/example/demo500px/network/PxApi.java
+++ Demo500Px/app/src/main/java/com/example/demo500px/network/PxApi.java
@@ -2,14 +2,20 @@ package com.example.demo500px.network;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
+import com.example.demo500px.network.model.Photo;
+import com.example.demo500px.network.model.Photos;
+import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.async.http.AsyncHttpRequest;
 import com.koushikdutta.async.http.Headers;
 import com.koushikdutta.ion.Ion;
+import com.koushikdutta.ion.Response;
 import com.koushikdutta.ion.loader.AsyncHttpRequestFactory;
 
 import android.content.Context;
 import android.net.Uri;
+import android.support.v4.app.Fragment;
 
 /**
  * @author Kanghee
@@ -20,6 +26,8 @@ public class PxApi {
     private static final String CONSUMER_SECRET = "1o14CfHPaH3iNbJ9zMKcsTi0bcGiPWsHYIzYWtAv";
 
     private static final String API_ENDPOINT_PHOTOS = "/photos";
+
+    private static final TypeToken<Photos> TYPE_TOKEN_PHOTOS = new TypeToken<Photos>() {};
 
     public static void initHttpClient(Context context) {
 
@@ -40,4 +48,22 @@ public class PxApi {
                 })
                 .setGson(gson);
     }
+
+    public static void getPhotos(Fragment fragment, int page, FutureCallback<Response<Photos>> callback) {
+        Uri.Builder uriBuilder = Uri.parse(HOST + API_ENDPOINT_PHOTOS)
+                .buildUpon();
+        Uri uri = uriBuilder.appendQueryParameter("consumer_key", CONSUMER_KEY)
+                .appendQueryParameter("feature", "popular")
+                .appendQueryParameter("page", String.valueOf(page))
+                .appendQueryParameter("image_size",
+                        Photo.ImageFormat.SIZE_440 + "," + Photo.ImageFormat.SIZE_1080)
+                .build();
+
+        Ion.with(fragment)
+                .load("GET", uri.toString())
+                .as(TYPE_TOKEN_PHOTOS)
+                .withResponse()
+                .setCallback(callback);
+    }
+
 }
